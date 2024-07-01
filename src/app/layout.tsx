da58839a70030fb07435c0e90e9e1429f28c2346
Suhayb Walton <s3842119@student.rmit.edu.au>
2024-07-01 11:18:10 +1000

    Fixed build error and added a toaster file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import Providers from '@/components/Providers'
 import Navbar from '@/components/Navbar'
 import { cn, constructMetadata } from '@/lib/utils'
 import { Inter } from 'next/font/google'
-import '@/app/globals.css';
+import '@/app/globals.css'
+import 'react-loading-skeleton/dist/skeleton.css'
+import 'simplebar-react/dist/simplebar.min.css'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,25 +13,27 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = constructMetadata()
 
 export default function RootLayout({
- children,
+  children,
 }: {
- children: React.ReactNode
+  children: React.ReactNode
 }) {
- return (
-   <html lang='en' className='light'>
-     {/* Wrap the application with the Providers component */}
-     <Providers>
-       <body
-         className={cn(
-           'min-h-screen font-sans antialiased grainy',
-           inter.className
-         )}>
-         {/* Render the Navbar component */}
-         <Navbar />
-         {/* Render the children components */}
-         {children}
-       </body>
-     </Providers>
-   </html>
- )
+  return (
+    <html lang='en' className='light'>
+      {/* Wrap the application with the Providers component */}
+      <Providers>
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased grainy',
+            inter.className
+          )}>
+          {/* Render the Toaster component */}
+          <Toaster />
+          {/* Render the Navbar component */}
+          <Navbar />
+          {/* Render the children components */}
+          {children}
+        </body>
+      </Providers>
+    </html>
+  )
 }
