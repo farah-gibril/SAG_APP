65a33802f5c0f696e09d48020b4b4cf66b95a3dd
farah-gibril <82480415+farah-gibril@users.noreply.github.com>
2025-07-01 20:14:50 +1000

    Update stripe.ts - admin fee
    new price (stripe fees to SAG has increased)

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -13,11 +13,11 @@ export const PLANS = [
   {
     name: 'Membership 150',
     slug: 'membership-150',
-    quota: 152.85,
+    quota: 152.90,
     price: {
       priceIds: {
-        test: 'price_1PY87uGnUCJjsBpA4ROGd8cb',
-        production: 'price_1PY87uGnUCJjsBpA4ROGd8cb',
+        test: 'price_1Rg17xGnUCJjsBpAGmy1n1Ex',
+        production: 'price_1Rg17xGnUCJjsBpAGmy1n1Ex',
       },
     },
   },
